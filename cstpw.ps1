d5fd603ea563c0e9a7ae38897d117371ca794e01
mhtvsSFrpHdE <2bemperor47@gmail.com>
2019-05-10 19:05:13 +0800

    Create cstpw.ps1
    - Initial commit.

---
+++ cstpw.ps1
@@ -0,0 +1,21 @@
+﻿# Set cmd script file location
+# Either full path or relative path should work
+# But put the variable out side the script, and change variable name if needed.
+# $CMD_SCRIPT_FILE = "C:\demo.cmd"
+
+# Create a empty cmd script file by using UTF-8 format hack
+function CreateCmdScript {
+    # Create a empty file
+    New-Item -Path "$CMD_SCRIPT_FILE" -ItemType File -Force | Out-Null
+    # This meanless line to trigger a common type error
+    # But it can let cmd.exe ignore the unsupported UTF-8 "BOM"
+    "gUsJAzrtybEx >nul 2>nul" | Out-File -LiteralPath "$CMD_SCRIPT_FILE" -Encoding UTF8
+}
+# Append content to the UTF-8 script
+function WriteCmdScript {
+    param (
+        $string
+    )
+    
+    $string | Add-Content -LiteralPath "$CMD_SCRIPT_FILE" -Encoding UTF8
+}
